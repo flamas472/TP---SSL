ca161aa93acfad8b91670f1652171a906f80b9e6
Federico Lamas <federicolamas95@gmail.com>
2018-09-02 11:11:03 -0300

    optimizaciones

--- main.c
+++ main.c
@@ -29,16 +29,16 @@ int tt[TAMF][TAMC] =  { // Tabla de Transiciones
 
 // FUNCIONES
 
-int menuPrincipal();
-void mostrarTT();
-void reconocedorDePalabras(int contador);
-int actualizarEstado(int estado, int caracter);
-int consultarCaracter(char caracter);
-int hacerTransicion(int estado, char caracter);
-int esEstadoFinal(int estado);
-int tomarLinea(char s[], int lim);
-int scanfIntervalo(int limInf, int limSup);
-int algoritmo3(char lineaDeTexto[MAXLINE], int contador);
+int menuPrincipal(); // Imprime el menu principal y retorna la poción ingresada por consola
+void mostrarTT(); // Imprime por consola la TT
+void reconocedorDePalabras(int contador); // corre un reconocedor que aplica el algoritmo3 mientras el usuario lo desee
+void tomarLinea(char s[MAXLINE], int lim); // Solicita por consola una linea de texto
+int scanfIntervalo(int limInf, int limSup); // Solicita por consola un entero verificando que pertenezca al intervalo
+int algoritmo3(char lineaDeTexto[MAXLINE], int contador); // Implementa el algoritmo3 en la linea de texto recibida por parámetro
+int hacerTransicion(int estado, char caracter); // recibe un estado y un caracter, y hace la transición al estado siguiente según corresponda
+int consultarCaracter(char caracter); // Recibe un caracter, y retorna el valor de la columna en la TT
+int actualizarEstado(int estado, int caracter); // Recibe un Estado y el valor de la columna correspondiente al caracter y retorna el siguiente estado
+int esEstadoFinal(int estado); // consulta si el estado recibido por parámetro es un estado final, de aceptación
 
 
 int main() {
@@ -78,7 +78,40 @@ int main() {
   return 0;
 }
 
-void reconocedorDePalabras(int contador){
+int menuPrincipal(){ // Imprime el menu principal y retorna la poción ingresada por consola
+  int opcion = 0;
+  printf("Menu Principal\n");
+  printf("\n");
+  printf(ER_MENSAJE);
+  printf("\n");
+  printf("1) Ver tabla de transiciones.\n");
+  printf("2) Ejecutar reconocedorde secuencias de cadenas.\n");
+  printf("0) Salir");
+  printf("\n");
+  printf("-> ");
+  scanf("%d", &opcion);
+  return opcion;
+}
+
+void mostrarTT(){ // Muestra la tabla de transiciones
+  int f, c;
+  printf("Tabla de Transiciones\n\n");
+  printf(ER_MENSAJE);
+  printf("\nTT\t\t0,1\t2-9\tB\t.\totro\t#\tfdt\n");
+  for(f=0; f<TAMF; f++){
+    printf("\nestado%d\t", f);
+    for(c=0; c<TAMC; c++){
+      if(tt[f][c]>TAMF){
+        printf("\t%c", tt[f][c]);
+      }else{
+        printf("\t%d", tt[f][c]);
+      }
+    }
+  }
+  printf("\n\n");
+}
+
+void reconocedorDePalabras(int contador){ // corre un reconocedor que aplica el algoritmo3 mientras el usuario lo desee
   char lineaDeTexto[100];
   int eleccion;
 
@@ -117,6 +150,23 @@ void reconocedorDePalabras(int contador){
       }
     }
   }while(eleccion != 0);
+}
+
+void tomarLinea(char s[MAXLINE], int lim){ // Solicita por consola una linea de texto
+  printf("Ingrese una cadena:\n");
+  printf("-> ");
+  scanf("%s", s);
+}
+
+int scanfIntervalo(int limInf, int limSup){ // Solicita por consola un entero verificando que pertenezca al intervalo
+  int eleccion;
+  scanf("%d", &eleccion);
+  while(eleccion<limInf || eleccion>limSup){
+    printf("Opcion incorrecta\n");
+    printf("-> ");
+    scanf("%d", &eleccion);
+  }
+  return eleccion;
 }
 
 int algoritmo3(char lineaDeTexto[MAXLINE], int contador){                         //ALGORITMO 3
@@ -158,86 +208,13 @@ int algoritmo3(char lineaDeTexto[MAXLINE], int contador){                       
   return contador;
 }                                                                                 // FIN DEL ALGORITMO 3
 
-int scanfIntervalo(int limInf, int limSup){
-  int eleccion;
-  scanf("%d", &eleccion);
-  while(eleccion<limInf || eleccion>limSup){
-    printf("Opcion incorrecta\n");
-    printf("-> ");
-    scanf("%d", &eleccion);
-  }
-  return eleccion;
+int hacerTransicion(int estado, char caracter){ // recibe un estado y un caracter, y hace la transición al estado siguiente según corresponda
+  int columna = consultarCaracter(caracter);
+  estado = actualizarEstado(estado, columna);
+  return estado;
 }
 
-int menuPrincipal(){
-  int opcion = 0;
-  printf("Menu Principal\n");
-  printf("\n");
-  printf(ER_MENSAJE);
-  printf("\n");
-  printf("1) Ver tabla de transiciones.\n");
-  printf("2) Ejecutar reconocedorde secuencias de cadenas.\n");
-  printf("0) Salir");
-  printf("\n");
-  printf("-> ");
-  scanf("%d", &opcion);
-  return opcion;
-}
-
-void mostrarTT(){ // Muestra la tabla de transiciones
-  int f, c;
-  printf("Tabla de Transiciones\n\n");
-  printf(ER_MENSAJE);
-  printf("\nTT\t\t0,1\t2-9\tB\t.\totro\t#\tfdt\n");
-  for(f=0; f<TAMF; f++){
-    printf("\nestado%d\t", f);
-    for(c=0; c<TAMC; c++){
-      if(tt[f][c]>TAMF){
-        printf("\t%c", tt[f][c]);
-      }else{
-        printf("\t%d", tt[f][c]);
-      }
-    }
-  }
-  printf("\n\n");
-}
-
-int esEstadoFinal(int estado){ // Devuelve 1 si el estado es final (segun vector estadoFinal[])
-  int i;
-  for(i=0; i<CANTIDADEF; i++){
-    if(estadoFinal[i]==estado){
-      return 1;
-    }
-  }
-  return 0;
-}
-
-int copiar(char a[], char desde[]){ // Copia la cadena desde[] en a[]
-  int i;
-  i = 0;
-  while((a[i] = desde[i]) != '\0')
-   ++i;
-  a[i+1] = '\0';
-  return i;
-}
-
-int tomarLinea(char s[], int lim){ // Solicita una cadena por entrada en consola
-  int longitud;
-  char str[lim];
-  printf("Ingrese una cadena:\n");
-  printf("-> ");
-  scanf("%s", &str);
-  longitud = copiar(s, str);
-  return longitud;
-}
-
-int actualizarEstado(int estado, int caracter){ // Recibe un estado y un caracter y devuelve el estado siguiente
-  int nuevoEstado;
-  nuevoEstado = tt[estado][caracter];
-  return nuevoEstado;
-}
-
-int consultarCaracter(char caracter){ // Consulta el caracter, retorna la columna de la TT
+int consultarCaracter(char caracter){ // Recibe un caracter, y retorna el valor de la columna en la TT
   switch(caracter){
     case '0': case '1':
       return 0;
@@ -263,8 +240,18 @@ int consultarCaracter(char caracter){ // Consulta el caracter, retorna la column
   }
 }
 
-int hacerTransicion(int estado, char caracter){ // Retorna el nuevo estado solicitando estado y caracter
-  int columna = consultarCaracter(caracter);
-  estado = actualizarEstado(estado, columna);
-  return estado;
+int actualizarEstado(int estado, int caracter){ // Recibe un Estado y el valor de la columna correspondiente al caracter y retorna el siguiente estado
+  int nuevoEstado;
+  nuevoEstado = tt[estado][caracter];
+  return nuevoEstado;
+}
+
+int esEstadoFinal(int estado){ // consulta si el estado recibido por parámetro es un estado final, de aceptación
+  int i;
+  for(i=0; i<CANTIDADEF; i++){
+    if(estadoFinal[i]==estado){
+      return 1;
+    }
+  }
+  return 0;
 }
